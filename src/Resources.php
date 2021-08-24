90a0f71d854c49a68a8de6ee0e7b992362c96a68
Eugeny Nosenko <Imnosok@gmail.com>
2021-08-24 11:45:54 +0300

    Update Resources.php

--- src/Resources.php
+++ src/Resources.php
@@ -57,12 +57,12 @@ class Resources implements ResourcesInterface, IteratorAggregate
     {
         $this->data = [];
         $resources = $this->getClient()->get($this->getUrl(), $query);
-        if(isset($resources['body']['meta'])) $this->meta = $resources['meta'];
-        if(isset($resources['body']['included'])) $this->included = $resources['included'];
+        if(isset($resources['body']->meta)) $this->meta = (array)$resources['body']->meta;
+        if(isset($resources['body']->included)) $this->included = (array)$resources['body']->included;
 
-        if(isset($resources['body']['data'])) {
-            foreach ($resources['body']['data'] as $resource) {
-                $this->data[] = new Resource($this->getClient(), $this->getUrl(), ['data' => $resource]);
+        if(isset($resources['body']->data)) {
+            foreach ($resources['body']->data as $resource) {
+                $this->data[] = new Resource($this->getClient(), $this->getUrl(), ['data' => (array)$resource]);
             }
         }
         return $this->data;
